6d6be692812b23d95b98c180352acc82f84605b9
ACA <165293103+KelompokACA@users.noreply.github.com>
2024-06-24 02:42:03 +0700

    Add files via upload

---
+++ proses_pembelian.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style> 
+    body {
+        background-color: #f7f7f7;
+    }
+
+    .container {
+        margin-top: 50px;
+        margin-bottom: 50px;
+    }
+
+    .card {
+        margin-bottom: 20px;
+        border: none;
+        box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+    }
+
+    .card-header {
+        background-color: #4d0404;
+        color: white;
+        font-weight: bold;
+    }
+
+    .card-body {
+        background-color: white;
+    }
+
+    .form-group label{ 
+        font-weight: bold;
+    }
+</style>
+</head>
+
+<body>
+<div class= "container">
+    <div class= "row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h1 style="text-align:center;">Terima Kasih!</h1>
+                </div>
+                <div class="card-body">
+    
+    <a href = "pembelian.html" class="btn btn-primary">Kembali ke Pembelian Tiket</a>
+</html>
